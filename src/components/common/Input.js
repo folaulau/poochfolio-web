ba3ce420f4138f69891275eb62cb9f7559edb09e
Saia Fonua <saiafonua@gmail.com>
2022-04-18 23:47:45 -0600

    add autocomplete google react api

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,14 +1,25 @@
-const Input = ({ labelText, placeholderText, type }) => {
-    return (
-        <div className="mb-5">
-           <label htmlFor={type} className="text-[15px] text-[#666666] ml-2 font-Museo-Sans-Rounded-700">
-                {labelText}
-            </label>
-            <div className="mt-3.5">
-                <input type={type} name={type} id={type} className="shadow-sm block w-full p-3 rounded-full w-80 text-[15px] text-[#a1a1a1] font-Museo-Sans-Rounded-500 bg-red-[#f1f7ff]" placeholder={placeholderText} style={{ border: '1px solid #85d8e7' }} />
-            </div>
-        </div>
-    )
-}
+const Input = ({ labelText, placeholderText, type, required = false }) => {
+  return (
+    <div className="mb-5">
+      <label
+        htmlFor={type}
+        className="text-[15px] text-[#666666] ml-2 font-Museo-Sans-Rounded-700"
+      >
+        {labelText}
+      </label>
+      <div className="mt-3.5">
+        <input
+          type={type}
+          name={type}
+          id={type}
+          required={required ? true : false}
+          className="shadow-sm block w-full p-3 rounded-full w-80 text-[15px] text-[#a1a1a1] font-Museo-Sans-Rounded-500 bg-red-[#f1f7ff]"
+          placeholder={placeholderText}
+          style={{ border: "1px solid #85d8e7" }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Input;
