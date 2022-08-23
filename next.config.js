3f725bdb4905c0be20c496b5ffb02f3fb20f38a5
techlead95 <techlead95@gmail.com>
2022-08-23 00:58:19 -0400

    Add build id

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   distDir: "build",
+  generateBuildId: async () => {
+    return new Date().getTime().toString()
+  },
 }
 
 module.exports = nextConfig
